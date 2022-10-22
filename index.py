688e475eaebb22415e07ada4c3a1d1fd24e482c1
Roman Rey <rey.roman23@gmail.com>
2022-10-22 17:07:36 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,34 @@
+import json
+from twilio.rest import Client
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+#Creamos objeto Flask
+app = Flask(__name__)
+
+#Cargamos la informacion de nuestro archivo config.json
+f = open("config.json", "r")
+env = json.loads(f.read())
+
+#Creamos nuestro primer servicio web
+@app.route('/test', methods=['GET'])
+def test():
+return "hello world"
+
+@app.route('/send_sms', methods=['POST'])
+def send_sms():
+try:
+account_sid = env['TWILIO_ACCOUNT_SID']
+auth_token = env['TWILIO_AUTH_TOKEN']
+origen = env['TWILIO_PHONE_NUMBER']
+client = Client(account_sid, auth_token)
+data = request.json
+contenido = data["contenido"]
+destino = data["destino"]
+message = client.messages.create(
+body=contenido,
+from_=origen,
+
+#Ejecutamos el servidor
+if __name__ == '__main__':
+app.run()
